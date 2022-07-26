b4fd9cac5250dc8eeb27e970609860c50688f2fd
kahbodaeini <kahbodaeini@gmail.com>
2022-07-26 11:12:07 +0430

    assignment3 social done

---
+++ interface/socialhealth/retriever/bridge/logic/social/query_exansion/query_expansion.py
@@ -0,0 +1,33 @@
+import random
+from nltk.corpus import wordnet as wn
+
+
+def query_expansion(query: str) -> list[str]:
+    query = query
+    query_list = [i.lower() for i in query.split()]
+
+    query_syn = {}
+    ret = {}
+
+    for word in query_list:
+        query_syn[word] = []
+        for syn in wn.synsets(word):
+            query_syn[word].append(syn.lemmas()[0].name().lower())
+        ret[word] = [*set(query_syn[word])]
+        try:
+            ret[word].remove(word)
+        except:
+            pass
+    print(ret)
+    expanded_queries = []
+    for i in range(3):
+        temp = ""
+        for word in query_list:
+            if len(ret[word]) > 0:
+                temp += random.choice(ret[word]) + " "
+            else:
+                temp += word + " "
+
+        expanded_queries.append(temp[:-1])
+
+    return expanded_queries
