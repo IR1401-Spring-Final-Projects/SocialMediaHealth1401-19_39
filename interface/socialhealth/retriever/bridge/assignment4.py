1bf53cedd7d4fa1d3d28df51bbc1ef433ab00ae6
mmdrez4 <mohammadreza.dn80@gmail.com>
2022-07-27 19:05:53 +0430

    final update for assignment4.py

--- interface/socialhealth/retriever/bridge/assignment4.py
+++ interface/socialhealth/retriever/bridge/assignment4.py
@@ -1,9 +1,11 @@
 from .logic.health.retrieval import classification as health_classify
 from .logic.health.retrieval import clustering as health_cluster
+from .logic.social.class_cluster import social_classification as social_classify
+from .logic.social.class_cluster import social_clustering as social_cluster
 
 
 def classify_cluster(subject: bool, search_term: str):
     if subject:  # health
         return health_classify.run(search_term), health_cluster.run(search_term)
     else:  # social
-        pass
+        return social_classify.classification(search_term), social_cluster.cluster(search_term)
