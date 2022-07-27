6c29427e0f17f25e76b9cbb1a5e8c98e79e4167a
mmdrez4 <mohammadreza.dn80@gmail.com>
2022-07-27 19:03:51 +0430

    update assignment4.py

--- interface/socialhealth/retriever/bridge/assignment4.py
+++ interface/socialhealth/retriever/bridge/assignment4.py
@@ -1,5 +1,9 @@
+from .logic.health.retrieval import classification as health_classify
+from .logic.health.retrieval import clustering as health_cluster
+
+
 def classify_cluster(subject: bool, search_term: str):
     if subject:  # health
-        pass
+        return health_classify.run(search_term), health_cluster.run(search_term)
     else:  # social
         pass
