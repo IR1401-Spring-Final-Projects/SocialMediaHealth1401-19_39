9f0e359da009168d96a9ed3cc61b2eeafbb514f2
Parsa Mohammadian <parsa2820@gmail.com>
2022-07-27 20:25:27 +0430

    Fix classify and cluster

--- interface/socialhealth/retriever/urls.py
+++ interface/socialhealth/retriever/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('link-analysis/', views.link_analysis, name='retriever-link-analysis'),
     path('link-analysis/social/', views.link_analysis_social, name='retriever-link-analysis-social'),
     path('link-analysis/health/', views.link_analysis_health, name='retriever-link-analysis-health'),
+    path('classify/health/', views.classify_health, name='retriever-classify-health'),
+    path('classify/social/', views.classify_social, name='retriever-classify-social'),
 ]
